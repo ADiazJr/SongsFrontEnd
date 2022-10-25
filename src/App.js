224a41620c116a7f4d3a53bcff3f58c01ca866ea
unknown <arturodiazmay@gmail.com>
2022-10-25 16:04:59 -0500

    Created React Project + Added MusicTable Component With Table and Axios GET Call

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import MusicTable from "./Components/MusicTable";
+
+function App() {
+
+  const [songs, setSongs] = useState([]);
+
+  useEffect(() => {
+    getAllSongs();
+  }, [])
+
+  async function getAllSongs(){
+    let response = await axios.get('http://127.0.0.1:8000/api/music/');
+    setSongs(response.data);
+    console.log(response.data)
+  }
+
+  return (
+    <div>
+      <MusicTable SongList={songs} />
+    </div>
+  );
+}
+
+export default App;
